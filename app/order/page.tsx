7adbd9a66119af1a5cefad22f04e1cac44caf089
Fatima Mendoza <20030105@itcelaya.edu.mx>
2024-05-31 21:39:14 -0600

    creando los contenedores principales

---
+++ app/order/page.tsx
@@ -0,0 +1,5 @@
+export default function Orderpage() {
+    return (
+      <h1>Orderpage</h1>
+    );
+  }
---
+++ app/products/new/page.tsx
@@ -0,0 +1,7 @@
+export default function CreateProductsPage() {
+console.log('Desde el servidor')
+
+    return (
+        <div>CreateProductsPage</div>
+    )
+}
