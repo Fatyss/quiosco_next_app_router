81ca305e64f7e1d0d8a0949cb292bb828da5e5e2
Fatima Mendoza <20030105@itcelaya.edu.mx>
2024-06-01 20:59:25 -0600

    implementación del routing dinamico en categoría, se agregaron los iconos

---
+++ app/order/[category]/page.tsx
@@ -0,0 +1,6 @@
+export default function Orderpage({params}: {params:{category: string}}) {
+  console.log(params.category)
+    return (
+      <h1>Orderpage</h1>
+    );
+  }
--- app/order/page.tsx
+++
@@ -1,5 +0,0 @@
-export default function Orderpage() {
-    return (
-      <h1>Orderpage</h1>
-    );
-  }
