a16a8738c6e57dfe77c4b38803a55817677b3870
Fatima Mendoza <20030105@itcelaya.edu.mx>
2024-06-01 22:25:24 -0600

    se implementaron los componentes que muestran los productos y con imágenes, nombre y precio

--- app/order/[category]/page.tsx
+++ app/order/[category]/page.tsx
@@ -1,6 +1,31 @@
-export default function Orderpage({params}: {params:{category: string}}) {
-  console.log(params.category)
+import ProductCard from "@/components/products/ProductsCard";
+import { prisma } from "@/src/lib/prisma";
+
+async function getProducts(category: string) {
+  const products = await prisma.product.findMany({
+    where: {
+      category:{
+        slug:category
+      }
+    }
+  })
+  return products
+}
+
+export default async function Orderpage({params}: {params:{category: string}}) {
+  const products = await getProducts(params.category)
+  //console.log(products)
     return (
-      <h1>Orderpage</h1>
+      <>
+      <h1 className="text-2xl my-10">Elige y personaliza tu pedido a continuacion</h1>
+      <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 grap-4 items-start">
+        {products.map(product =>(
+          <ProductCard 
+          key={product.id}
+          product={product}
+          />
+        ))}
+      </div>
+      </>
     );
   }
