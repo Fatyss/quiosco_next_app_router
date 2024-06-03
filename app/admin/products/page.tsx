2686ba61304af549b7d04205d2f239b529c913e7
Fatima Mendoza <20030105@itcelaya.edu.mx>
2024-06-02 23:54:33 -0600

    seccion de productos ya muestra una tabla con los productos y la categoria a la que estan

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -1,11 +1,28 @@
+import ProductTable from "@/components/products/ProductsTable";
 import Heading from "@/components/ui/Heading";
+import { products } from "@/prisma/data/products";
+import { prisma } from "@/src/lib/prisma";
+async function getProducts() {
+    const products = await prisma.product.findMany({
+        include:{
+            category: true 
+        }
+    })
+    return products
+}
 
-export default function ProductsPage() {
+export type ProductsWhithCategory = Awaited<ReturnType<typeof getProducts>>
+
+export default async function ProductsPage() {
+    const products= await getProducts()
+
     return (
         <>
         <Heading>Administrar Productos</Heading>
         
-        
+        <ProductTable 
+            products={products}
+        />
         </>
     )
 }
