c7569031a4c2e5b996fc3965ec14ec75401c632b
Fatima Mendoza <20030105@itcelaya.edu.mx>
2024-06-02 23:22:23 -0600

    seccion de ordenes, se termino de configurar el boton de completado, se arreglo el problema de que se eliminen las ordenes que se completan

--- components/order/OrderCard.tsx
+++ components/order/OrderCard.tsx
@@ -1,10 +1,11 @@
+import { completeOrder } from "@/actions/complete-order-action"
 import { OrderWhithProducts } from "@/src/types"
 import { formatCurrency } from "@/src/utils"
 
 type OrderCardProps = {
     order: OrderWhithProducts
 }
-export default function OrderCard({ order }:OrderCardProps) {
+export default function OrderCard({ order }: OrderCardProps) {
 
     return (
         <section
@@ -14,13 +15,13 @@ export default function OrderCard({ order }:OrderCardProps) {
             <p className='text-2xl font-medium text-gray-900'>Cliente: {order.name} </p>
             <p className='text-lg font-medium text-gray-900'>Productos Ordenados:</p>
             <dl className="mt-6 space-y-4">
-                {order.orderProducts.map(product=>(
+                {order.orderProducts.map(product => (
                     <div
                         key={product.productId}
                         className="flex items-center gap-2 border-t border-gray-200 pt-4"
                     >
                         <dt className="flex item-center text-sm text-gray-600">
-                        <span className="font-black">({product.quantity}) {''} </span>
+                            <span className="font-black">({product.quantity}) {''} </span>
                         </dt>
                         <dd className="text-sm font-medium text-gray-900">{product.product.name} </dd>
 
@@ -34,7 +35,12 @@ export default function OrderCard({ order }:OrderCardProps) {
                 </div>
             </dl>
 
-            <form>
+            <form action={completeOrder}>
+                <input
+                    type="hidden"
+                    value={order.id}
+                    name="order_id"
+                />
                 <input
                     type="submit"
                     className="bg-indigo-600 hover:bg-indigo-800 text-white w-full mt-5 p-3 uppercase font-bold cursor-pointer"
