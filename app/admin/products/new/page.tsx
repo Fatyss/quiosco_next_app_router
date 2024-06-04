5136635b552e9fb2b56e6b0da782bf9513275f4e
Fatima Mendoza <20030105@itcelaya.edu.mx>
2024-06-04 00:12:17 -0600

    Se agrego un formulario para crear un producto, aun falta arreglar que muestre las categorias y que se pueda agregar una foto

--- app/admin/products/new/page.tsx
+++ app/admin/products/new/page.tsx
@@ -1,7 +1,13 @@
+import AddProductForm from "@/components/products/AddProductForm"
+import Heading from "@/components/ui/Heading"
+
 export default function CreateProductsPage() {
 console.log('Desde el servidor')
 
     return (
-        <div>CreateProductsPage</div>
+        <>
+        <Heading>Nuevo Producto</Heading>
+        <AddProductForm/>
+        </>
     )
 }
