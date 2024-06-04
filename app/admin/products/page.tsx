fe39df6bf8cdc6e220b9a5b05f1c4f08be29cad0
Fatima Mendoza <20030105@itcelaya.edu.mx>
2024-06-03 22:59:10 -0600

    Se arreglo la paginaciones de productos, ya se cuenta con botones de anterior y siguiente ademas de numeracion de en que pagina se encuentra

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -1,9 +1,20 @@
+import { redirect } from "next/navigation";
+import ProductPagination from "@/components/products/ProductsPagination";
 import ProductTable from "@/components/products/ProductsTable";
 import Heading from "@/components/ui/Heading";
 import { products } from "@/prisma/data/products";
 import { prisma } from "@/src/lib/prisma";
-async function getProducts() {
+
+async function productCount() {
+    return await prisma.product.count()
+}
+
+
+async function getProducts(page: number, pageSize: number) {
+    const skip = (page -1)*pageSize
     const products = await prisma.product.findMany({
+        take: pageSize,
+        skip: skip,
         include:{
             category: true 
         }
@@ -13,15 +24,31 @@ async function getProducts() {
 
 export type ProductsWhithCategory = Awaited<ReturnType<typeof getProducts>>
 
-export default async function ProductsPage() {
-    const products= await getProducts()
+export default async function ProductsPage({searchParams}: {searchParams: {page: string}}) {
+    
+    const page = +searchParams.page || 1
+    const pageSize = 10
+    
+    if(page< 0 ) redirect('/admin/products')
 
+    const productsData=  getProducts(page, pageSize)
+    const totalProductsData =  productCount()
+    const [products, totalProducts ] = await Promise.all([productsData,totalProductsData])
+    const totalPages = Math.ceil(totalProducts / pageSize)
+   
+    if(page> totalPages )redirect('/admin/products')
+   
+   
     return (
         <>
         <Heading>Administrar Productos</Heading>
         
         <ProductTable 
             products={products}
+        />
+        <ProductPagination 
+            page={page}
+            totalPages = {totalPages}
         />
         </>
     )
